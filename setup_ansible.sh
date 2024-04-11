562e942905d5b3a6ffbab114a5159f9a03bc0c4d
kelevrablondie <mykhailo.zaichkov@gmail.com>
2024-04-11 19:31:50 +0200

    Changes to setup script, added generate-key playbook, changes to security playbook

--- setup_ansible.sh
+++ setup_ansible.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 # Function to install Ansible
 install_ansible() {
     echo "Installing Ansible..."
@@ -7,15 +8,20 @@ install_ansible() {
     sudo apt-add-repository --yes --update ppa:ansible/ansible
     sudo apt install -y ansible
     echo "Ansible installed successfully."
+}
+
+# Determine the directory of the script
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 
 # Check if Ansible is installed
-if ! command -v ansible &> /dev/null
-then
+if ! command -v ansible &> /dev/null; then
     install_ansible
 else
     echo "Ansible is already installed."
 fi
+
 # Change directory to the cloned repo
-cd /ansible-secure-server
+cd "$SCRIPT_DIR"
+
 # Run the playbook
 ansible-playbook secure_server_setup.yml
